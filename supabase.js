1b741693530da0caa29b78f0dbaa3e051aac4869
dashboard-sedam <68096665+projetosetags@users.noreply.github.com>
2026-06-24 10:49:22 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,3 @@
-
+const SUPABASE_URL='https://vlpppyaezaihchhicmxu.supabase.co'
+const SUPABASE_KEY='sb_publishable_D8YfhO5P7Dd3o_rd6cLfuQ_VrfX6MEG'
+const client=supabase.createClient(SUPABASE_URL,SUPABASE_KEY)
