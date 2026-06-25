7a025b2b8efbf2d6c0e338ac62d19be6d905a0c8
dashboard-sedam <68096665+projetosetags@users.noreply.github.com>
2026-06-25 10:45:48 -0300

    Add caching functionality to service worker
    Implement service worker caching for files.

--- sw.js
+++ sw.js
@@ -1,2 +1,26 @@
-self.addEventListener('install',e=>self.skipWaiting())
-self.addEventListener('fetch',()=>{})
+const CACHE_NAME='barbearia-ld-v1'
+const ARQUIVOS=[
+'./',
+'./index.html',
+'./cliente.html',
+'./admin.html',
+'./style.css',
+'./app.js',
+'./supabase.js',
+'./manifest.json',
+'./barbeariald.png',
+'./favicon.ico',
+'./android-chrome-192x192.png',
+'./android-chrome-512x512.png'
+]
+self.addEventListener('install',e=>{
+e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ARQUIVOS)))
+self.skipWaiting()
+})
+self.addEventListener('activate',e=>{
+e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))))
+self.clients.claim()
+})
+self.addEventListener('fetch',e=>{
+e.respondWith(caches.match(e.request).then(resp=>resp||fetch(e.request)))
+})
