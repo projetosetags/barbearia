4ef16c647e5670469d54ffae71c1181d0c65d488
dashboard-sedam <68096665+projetosetags@users.noreply.github.com>
2026-06-26 20:27:34 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,12 @@
-const CACHE_NAME='barbearia-ld-v1'
-const ARQUIVOS=[
-'./',
-'./index.html',
-'./cliente.html',
-'./admin.html',
-'./style.css',
-'./app.js',
-'./supabase.js',
-'./manifest.json',
-'./barbeariald.png',
-'./favicon.ico',
-'./android-chrome-192x192.png',
-'./android-chrome-512x512.png'
-]
+const CACHE_NAME='barbearia-ld-v2'
 self.addEventListener('install',e=>{
-e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ARQUIVOS)))
 self.skipWaiting()
 })
 self.addEventListener('activate',e=>{
-e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))))
-self.clients.claim()
+e.waitUntil(
+caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+)
 })
 self.addEventListener('fetch',e=>{
-e.respondWith(caches.match(e.request).then(resp=>resp||fetch(e.request)))
+e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))
 })
