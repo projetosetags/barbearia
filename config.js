9df208dde51736cbad841862b3ec709f2d32d824
dashboard-sedam <68096665+projetosetags@users.noreply.github.com>
2026-09-11 23:07:35 -0300

    Cria configuração central do novo projeto

---
+++ config.js
@@ -0,0 +1,20 @@
+window.BARBEARIA_CONFIG={
+  nome:'Barbearia Leandro David',
+  cidade:'Laguna/SC',
+  endereco:'Rua Coronel Fernandes Martins, 251, em frente à UDESC, Laguna - SC',
+  whatsapp:'5548996684751',
+  instagram:'',
+  inicio:'08:30',
+  fim:'20:00',
+  intervalo:30,
+  supabaseUrl:'',
+  supabasePublishableKey:'',
+  servicosPadrao:[
+    {id:'corte',nome:'Corte masculino',duracao:30,valor:40},
+    {id:'barba',nome:'Barba',duracao:30,valor:30},
+    {id:'combo',nome:'Corte + barba',duracao:60,valor:60}
+  ],
+  barbeirosPadrao:[
+    {id:'leandro-david',nome:'Leandro David'}
+  ]
+};
