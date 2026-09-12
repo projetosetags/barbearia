05e9fe5ef26fd291a4a88aa8e130c09372996eea
dashboard-sedam <68096665+projetosetags@users.noreply.github.com>
2026-09-12 13:05:18 -0300

    Add Firebase configuration to firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,9 @@
-window.FB_CONFIG={};
+window.FB_CONFIG = {
+  apiKey: "AIzaSyBzo9Kqd2j2cB_ApBh_Ob5JkiAkxnfdrAw",
+  authDomain: "barbearia-leandro-david.firebaseapp.com",
+  databaseURL: "https://barbearia-leandro-david-default-rtdb.firebaseio.com",
+  projectId: "barbearia-leandro-david",
+  storageBucket: "barbearia-leandro-david.firebasestorage.app",
+  messagingSenderId: "34716872252",
+  appId: "1:34716872252:web:4d9fbb0451f70142db1d39"
+};
